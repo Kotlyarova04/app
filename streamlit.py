78ccc3d33994b6b1f9352c9872334a1ca9be629a
Kotlyarova04 <125530657+Kotlyarova04@users.noreply.github.com>
2025-03-20 11:27:01 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -10,15 +10,22 @@ password = st.sidebar.text_input('Пароль', type  = 'password')
 if st.sidebar.button("Войти"):
     st.sidebar.success(f"Добро пожаловать, {username}!")
 
-
-st.subheader("База рейсов")
-
-
-
 flights = pd.DataFrame({
-    "Рейс": [f"S7 {random.randint(1000, 9999)}" for i in range(10)]})
+    "Рейс": [f"S7 {random.randint(1000, 9999)}" for i in range(10)],
+    "Откуда":[f".." for i in range(10)],
+    "Куда":[f".." for i in range(10)],
+    "Время вылета":[f".." for i in range(10)]})
 flights.index= range(1,len(flights)+1)
 
-st.dataframe(flights)
+tab1, tab2= st.tabs(["Главная","База рейсов"])
 
-st.subheader("✈️ Информация о рейсе")
+with tab2:
+    st.subheader("База рейсов")
+    st.dataframe(flights)
+
+with tab1:
+    st.subheader("Информация о рейсе")
+    selected_flight = st.selectbox("Выберите рейс:", flights["Рейс"])
+    flight_info = flights[flights["Рейс"] == selected_flight]
+    st.write(f"**Маршрут:** ")
+    st.write(f"**Время вылета:** ")
